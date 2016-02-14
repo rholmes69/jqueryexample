70c978335ec0f517fb1384886c8d1a12ef970b80
rholmes69 <roderick.sholmes@gmail.com>
2016-02-14 05:59:16 -0800

    added readme file

---
+++ jqueryexample.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+    jQuery Example
+    ~~~~~~~~~~~~~~
+
+    A simple application that shows how Flask and jQuery get along.
+
+    :copyright: (c) 2015 by Armin Ronacher.
+    :license: BSD, see LICENSE for more details.
+"""
+from flask import Flask, jsonify, render_template, request, url_for
+app = Flask(__name__)
+
+
+@app.route('/_add_numbers')
+def add_numbers():
+    """Add two numbers server side, ridiculous but well..."""
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def index2():
+    return render_template('index2.html')
+    
+if __name__ == '__main__':
+    app.run()
